cf757798209623999ee214fc546251ab9711f1d6
JacobHawk603 <62320033+JacobHawk603@users.noreply.github.com>
2023-05-06 01:18:58 -0600

    generador de correos aparentemente funcionando

--- src/main/java/com/empresa/proyecto/controllers/EmailController.java
+++ src/main/java/com/empresa/proyecto/controllers/EmailController.java
@@ -1,6 +1,7 @@
 package com.empresa.proyecto.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,9 +9,13 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.empresa.proyecto.models.entity.Usuario;
+import com.empresa.proyecto.models.service.IUsuarioService;
+import com.empresa.proyecto.models.service.UsuarioService;
 import com.empresa.proyecto.services.IEmailService;
 
 
+import java.security.SecureRandom;
 
 
 @RestController
@@ -24,12 +29,40 @@ public class EmailController {
 	IEmailService emailService;
 	
 	@Autowired
-	IUsuarioService usuario;
+	IUsuarioService usuarioService;
+	
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+	
+	private static String generarContrasena(int len) {
+		
+		// Rango ASCII – alfanumérico (0-9, a-z, A-Z)
+        final String chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+ 
+        SecureRandom random = new SecureRandom();
+        StringBuilder sb = new StringBuilder();
+ 
+        // cada iteración del bucle elige aleatoriamente un carácter del dado
+        // rango ASCII y lo agrega a la instancia `StringBuilder`
+ 
+        for (int i = 0; i < len; i++)
+        {
+            int randomIndex = random.nextInt(chars.length());
+            sb.append(chars.charAt(randomIndex));
+        }
+ 
+        return sb.toString();
+	}
 	
 	@GetMapping("/mail")
 	public void enviarMail() {
 		//System.out.println("Esto se esta ejecutando");
-		emailService.send("shinesadecv170@gmail.com", "cristobalavalos09@gmail.com", "pruebasSpringBoot", "Hola mundito desde spring email(>w<)");
+		Usuario usuario = null;
+		usuario = usuarioService.findByEmail("cristobalavalos09@gmail.com");
+		System.out.println(usuario.getNombre());
+		usuario.setPassword(passwordEncoder.encode(generarContrasena(10)));
+		Usuario nuevoUsuario = usuarioService.save(usuario);
+		//emailService.send("shinesadecv170@gmail.com", "cristobalavalos09@gmail.com", "pruebasSpringBoot", "Hola mundito desde spring email(>w<)");
 	}
 	
 	/*
